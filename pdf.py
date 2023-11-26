b672147cbaaf870810570dcfa0a05f5ca790f5cc
VA_24 <24agnihotriv@smtexas.org>
2023-11-26 03:23:26 -0600

    prometheus pdf read

--- pdf.py
+++ pdf.py
@@ -1,23 +1,39 @@
 import PyPDF2
 #scibowldb goes until 7694
 
-pdfFileObj = open('Olympus_DE5.pdf', 'rb')
+pdfFileObj = open('test.pdf', 'rb')
 pdfReader = PyPDF2.PdfReader(pdfFileObj)
 
-text = ""
+text = ''
 
 for pageNum in range(len(pdfReader.pages)):
     pageObj = pdfReader.pages[pageNum]
     text += pageObj.extract_text()
 
 pdfFileObj.close()
+question_dict = {i: [] for i in range(1, 25)}
 
-keywords = ['TOSS-UP', 'ANSWER', 'BONUS']
-sections = {keyword: [] for keyword in keywords}
+keywords = ['TOSS-UP', 'ANSWER:', 'BONUS']
+text = text.split('TOSS-UP')
+text.pop(0)
 
-for keyword in keywords:
-    temp = text.split(keyword)
-    sections[keyword] = temp.pop(0)
-    text = "".join(temp)
+for j in range(len(text)):
+    question = text[j].split('ANSWER')
+    question = question[0][2:].replace('\n', ' ')
 
-print(sections)
+    if question[3] != 'X' and question[4] != 'X':
+        question = question.split(' - ')
+
+        category_text = ''
+        category = question[0].split(' ')
+        category.pop(0)
+        for word in category:
+            category_text += word + ' '
+
+        if 'Short Answer' in question[1]:
+            question_type = 'Short Answer '
+            question_body = question[1].replace(question_type, '')
+            if category != 'Math':
+                print(question_body)
+        else:
+            question_type = 'Multiple Choice'
